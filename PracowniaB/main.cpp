50405f240de6ce6657cad603bfb4f97279ac5289
szewczykmira <szewczyk.mira@gmail.com>
2015-03-31 20:56:07 +0200

    Changes

--- PracowniaB/main.cpp
+++ PracowniaB/main.cpp
@@ -7,15 +7,15 @@
 using namespace std;
 
 const int MAXSUM=1000001;
-int suffix_sum = 0;
+int suffix_sum = 0, max_sum = 0;
 int pierwsza_niezerowa = MAXSUM;
 vector<int> lego;
-map<int, int> reszta;
-map<int, int> buffer;
+vector<int> reszta(MAXSUM+1, 0);
+vector<int> buffer(MAXSUM+1, 0);
 
 bool func(int i, int j)
 {
-    return (i>j);
+    return (i<j);
 }
 
 void read_data()
@@ -34,20 +34,15 @@ void buffer_push(const int a, const int b)
     const int mi = min(a,b);
     const int mx = max(a,b);
     const int tmp = mx - mi;
-    if(tmp < suffix_sum + pierwsza_niezerowa) {
-      if(buffer.find(tmp) != buffer.end()){
-        buffer[tmp] = max(mx, buffer[tmp]);
-      } else {
-        buffer[tmp] = mx;
-      }
+    if(tmp <= suffix_sum + pierwsza_niezerowa) {
+      buffer[tmp] = max(mx, buffer[tmp]);
     }
 }
 
 void flush_buffer()
 {
-  for(map<int, int>::iterator i=buffer.begin(); i != buffer.end(); ++i) {
-      int key = i->first;
-      reszta[key] = max(i->second, reszta[key]);
+  for(int key=0; key != buffer.size(); ++key) {
+      reszta[key] = max(buffer[key], reszta[key]);
       if(reszta[key] - key != 0 && key < pierwsza_niezerowa) {
           pierwsza_niezerowa = key;
       }
@@ -60,26 +55,32 @@ void crunch_data()
     sort(lego.begin(), lego.end(), func);
     reszta[0] = 0;
     for(int i=0; i<lego.size(); ++i) {
-        for(map<int, int>::iterator j=reszta.begin(); j != reszta.end(); ++j) {
-            int higher = j->second;
-            int lower = j->second - j->first;
-            buffer_push( higher + lego[i],
+        for(int j=0; j != reszta.size(); ++j) {
+            int higher = reszta[j];
+            int lower = higher - j;
+            if(lower >= 0 && j <= suffix_sum + pierwsza_niezerowa && j <= max_sum) {
+                printf("j: %d h: %d l: %d i: %d\n", j, higher, lower, lego[i]);
+                buffer_push( higher + lego[i],
                             lower);
 
-            buffer_push( lower + lego[i],
+                buffer_push( lower + lego[i],
                            higher);
+            }
         }
         flush_buffer();
+        printf("ms: %d ss: %d\n", max_sum, suffix_sum + pierwsza_niezerowa);
         suffix_sum -= lego[i];
+        max_sum += lego[i];
     }
 }
 
 void output_solution()
 {
   int tmp;
-    for(map <int, int>::iterator i=reszta.begin(); i != reszta.end(); ++i) {
-        if((i->second - i->first) != 0) {
-            tmp = i->first;
+    for(int i=0; i != reszta.size(); ++i) {
+        if((reszta[i] - i) != 0) {
+            tmp = i;
+            printf("%d - %d\n", i, reszta[i]);
             break;
         }
     }
